6c93bcb61e973f5fde2228cf6d078dca4c84db5c
everaldo@bb-8 <juniorklawa10@gmail.com>
2021-09-29 12:37:10 -0300

    wip

---
+++ src/hooks/auth.spec.tsx
@@ -0,0 +1,46 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import { logInAsync } from "expo-google-app-auth";
+import { mocked } from "ts-jest/utils";
+import { AuthProvider, useAuth } from "./auth";
+
+jest.mock("expo-google-app-auth");
+
+describe("Auth Hook", () => {
+  it("user should be able to sign in with Google Account", async () => {
+    const googleMocked = mocked(logInAsync as any);
+
+    googleMocked.mockReturnValueOnce({
+      type: "success",
+      user: {
+        id: "123",
+        email: "meuemail@email.com",
+        name: "Devair",
+        photo: "www.myphoto.com",
+      },
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user.email).toBe("meuemail@email.com");
+  });
+
+  it("user should not be able to sign in with Google Account if cancels auth", async () => {
+    const googleMocked = mocked(logInAsync as any);
+
+    googleMocked.mockReturnValueOnce({
+      type: "cancel",
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty("id");
+  });
+});
---
+++ src/hooks/auth.tsx
@@ -0,0 +1,135 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as AppleAuthentication from "expo-apple-authentication";
+import * as Google from "expo-google-app-auth";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+  signInWithGoogle(): Promise<void>;
+  signInWithApple(): Promise<void>;
+}
+
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+  const [userStorageLoading, setUserStorageLoading] = useState(true);
+
+  const userStorageKey = "@gofinances:user";
+
+  async function signInWithGoogle() {
+    try {
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const result = await Google.logInAsync({
+        iosClientId: "123",
+        androidClientId: "123",
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+
+
+        const userLogged = {
+          id: String(result.user.id),
+          email: result.user.email!,
+          name: result.user.name!,
+          photo: result.user.photoUrl!,
+        };
+
+        setUser(userLogged);
+        await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async function signInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+
+      if (credential) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+          photo: undefined,
+        };
+
+        setUser(userLogged);
+        await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  useEffect(() => {
+    async function loadUserStorageDate() {
+      const userStoraged = await AsyncStorage.getItem("@gofinances:user");
+
+      if (userStoraged) {
+        const userLogged = JSON.parse(userStoraged) as User;
+        setUser(userLogged);
+      }
+
+      setUserStorageLoading(false);
+    }
+
+    loadUserStorageDate();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle,
+        signInWithApple,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
